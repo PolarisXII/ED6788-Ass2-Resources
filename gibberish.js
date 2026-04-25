a60e5112a3530e95275c5f84771004f72e90d803
Sylvia Wang <sylviamiaowang@gmail.com>
2026-04-25 14:56:28 +1000

    gibberish

---
+++ gibberish.js
@@ -0,0 +1,58 @@
+
+// gibberish.js
+// Complete the three functions below.
+// Use the gibberish string provided.
+
+let gibberish = "bratalomivensakuporidafemutalopiragenosavitekulabemarino";
+
+// ----------------------------------------------------
+// Challenge 1:
+// Calculate the percentage of vowels in the string.
+// Vowels = a, e, i, o, u
+// Return the percentage as a number.
+// Example: 40 means 40%
+// ----------------------------------------------------
+function vowelPercentage(gibberish) {
+  // Write your code here
+
+}
+
+
+
+// ----------------------------------------------------
+// Challenge 2:
+// Count how many times the letter "a" appears.
+// Return the total count.
+// ----------------------------------------------------
+function countAs(gibberish) {
+  // Write your code here
+
+}
+
+
+
+// ----------------------------------------------------
+// Challenge 3:
+// Determine if the string contains the letter "z".
+// ----------------------------------------------------
+function hasZ(gibberish) {
+  // Write your code here
+
+}
+
+
+
+// ----------------------------------------------------
+// Test your answers below by uncommenting the lines and running this file with node.
+// ----------------------------------------------------
+// console.log("Gibberish String:");
+// console.log(gibberish);
+
+// console.log("1. Vowel Percentage:");
+// console.log(vowelPercentage(gibberish));
+
+// console.log("2. Number of a's:");
+// console.log(countAs(gibberish));
+
+// console.log("3. Contains z?");
+// console.log(hasZ(gibberish));
